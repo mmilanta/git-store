f6befc79a43078b10509af3548dddd03c233aa7d
marco-caplena <marco@caplena.com>
2024-06-09 16:39:54 +0200

    fix: push every n seconds

--- src/app.py
+++ src/app.py
@@ -1,13 +1,35 @@
 import os
 from fastapi import FastAPI, HTTPException, Request, Path
 from typing import Annotated
+import asyncio
+import logging
 
 import git_commands
 
-app = FastAPI()
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
 
 data_folder = os.environ["LOCAL_REPO_FOLDER"]
 KeyType = Annotated[str, Path(title="key", pattern="^[^/\0]+$")]
+
+
+PUSH_DUE: bool = False
+
+
+async def push_loop():
+    global PUSH_DUE
+    while True:
+        await asyncio.sleep(os.environ["PUSH_EVERY_N_SECONDS"])
+        if PUSH_DUE:
+            git_commands.push()
+            PUSH_DUE = False
+        else:
+            logger.info("skipping PUSH")
+
+asyncio.create_task(push_loop())
+
+app = FastAPI()
 
 
 @app.get("/{key}", status_code=200)
@@ -26,10 +48,12 @@ async def set_data(key: KeyType, request: Request) -> None:
         bystr = await request.body()
         f.write(bystr.decode("utf-8"))
 
-    git_commands.commit_and_push(
+    git_commands.commit(
         file_path=key,
         commit_message=("new" if is_new_file else "edit") + f": {key}",
     )
+    global PUSH_DUE
+    PUSH_DUE = True
 
 
 @app.delete("/{key}", status_code=200)
@@ -37,10 +61,12 @@ async def delete_data(key: KeyType) -> None:
     check_valid_path(key)
     os.remove(os.path.join(data_folder, key))
 
-    git_commands.commit_and_push(
+    git_commands.commit(
         file_path=None,
         commit_message=f"deleted: {key}"
     )
+    global PUSH_DUE
+    PUSH_DUE = True
 
 
 @app.get("/", status_code=200)
--- src/git_commands.py
+++ src/git_commands.py
@@ -16,11 +16,16 @@ repo.config_writer().set_value("user", "name", os.environ["USER_NAME"]).release(
 repo.config_writer().set_value("user", "email", os.environ["USER_EMAIL"]).release()
 
 
-def commit_and_push(file_path: str | None, commit_message: str):
+def commit(file_path: str | None, commit_message: str):
     if file_path is not None:
         repo.index.add([file_path])
     else:
         repo.git.add(u=True)
+    logger.info(f"committing {commit_message}")
     repo.index.commit(commit_message)
+
+
+def push():
     origin = repo.remote(name='origin')
+    logger.info("pushing to remote")
     origin.push()
